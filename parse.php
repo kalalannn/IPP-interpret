cc94bec099bb888b25b6fa3a4b60c37c083d38d7
kalalannn <xvorob00@stud.fit.vutbr.cz>
2019-02-15 14:44:17 +0100

    all tests

--- parse.php
+++ parse.php
@@ -17,7 +17,7 @@ xmlwriter_start_attribute($xw, 'language');
 xmlwriter_text($xw, 'IPPcode19');
 xmlwriter_end_attribute($xw);
 
-function instr_xml($instruction){
+function xml_instr($instruction){
 	global $xw, $instr_counter;
 	// Instruction
 	xmlwriter_start_element($xw, 'instruction');
@@ -25,10 +25,34 @@ function instr_xml($instruction){
 	xmlwriter_start_attribute($xw, 'order');
 	xmlwriter_text($xw, $instr_counter++);
 	xmlwriter_end_attribute($xw);
-
 	xmlwriter_start_attribute($xw, 'opcode');
 	xmlwriter_text($xw, $instruction);
 	xmlwriter_end_attribute($xw);
+}
+
+function xml_attr($attr, $number, $type){
+	global $xw;
+	xmlwriter_start_element($xw, 'arg'.$number);
+	xmlwriter_start_attribute($xw, 'type');
+	xmlwriter_text($xw, $type);
+	xmlwriter_end_attribute($xw);
+	xmlwriter_start_attribute($xw, 'text');
+	xmlwriter_text($xw, $attr);
+	xmlwriter_end_attribute($xw);
+	xmlwriter_end_element($xw);
+}
+
+function var_attr($attr, $number){
+	$arg = explode('@', $attr);
+	if(
+		(count($arg) != 2) || !(
+			in_array($arg[0], array('GF', 'TF', 'LF')) && 
+				is_label($arg[1]) == OK
+			)
+		)
+		return 22;
+	xml_attr($attr, $number, 'var');
+	return OK;
 }
 
 function next_attr(){
@@ -37,78 +61,37 @@ function next_attr(){
 		return _EMPTY;
 	return $x;
 }
-function attr_xml($attr, $number, $type){
-	global $xw;
-	xmlwriter_start_element($xw, 'arg'.$number);
-	xmlwriter_start_attribute($xw, 'type');
-	xmlwriter_text($xw, $type);
-	xmlwriter_end_attribute($xw);
 
-	xmlwriter_start_attribute($xw, 'text');
-	xmlwriter_text($xw, $attr);
-	xmlwriter_end_attribute($xw);
-	xmlwriter_end_element($xw);
-}
-
-function var_attr($attr, $number){
-	if($attr == _EMPTY)
-		return 22;
-	if(is_var($attr) != OK)
-		return 22;
-	attr_xml($attr, $number, 'var');
-	return OK;
-}
-
-
-function is_var($attr){
-	$arg = explode('@', $attr);
-	if(count($arg) != 2)
-		return 22;
-	if(in_array($arg[0], array('GF', 'TF', 'LF')) && is_label($arg[1]) == OK)
-		return OK;
-	else 
-		return 22;
-}
-
-function is_symb($attr, $number){
+function symb_attr($attr, $number){
 	$arg = explode('@', $attr);
 	if(count($arg) != 2)
 		return 22;
 	switch($arg[0]){
 		case 'int':
-			if(preg_match('/[0-9]*/', $arg[1]) != 1)
+			if(preg_match('/[0-9]*/', $arg[1]) != 1) 	//is_int
 				return 22;
-			attr_xml($arg[1], $number, $arg[0]);
 			break;
 		case 'bool':
-			if(!($arg[1] == 'true' || $arg[1] == 'false'))
+			if(!($arg[1] == 'true' || $arg[1] == 'false')) //is_bool
 				return 22;
-			attr_xml($arg[1], $number, $arg[0]);
 			break;
 		case 'nil':
-			if($arg[1] != 'nil')
+			if($arg[1] != 'nil')	//is_nil
 				return 22;
-			attr_xml($arg[1], $number, $arg[0]);
 			break;
 		case 'string':	// testovani v pythonu bude
-			attr_xml($arg[1], $number, $arg[0]);
 			break;
 		default:
-			if(in_array($arg[0], array('GF', 'TF', 'LF')))
-				return var_attr($attr, $number);
-			else 
-				return 22;
-			break;
-				
+			return var_attr($attr, $number);
 	}
+	xml_attr($arg[1], $number, $arg[0]);
 	return OK;
 }
 function label_attr($attr, $number){
 	if(is_label($attr) != OK)
 		return 22;
-	attr_xml($attr, $number, 'label');
+	xml_attr($attr, $number, 'label');
 	return OK;
-
 }
 
 function is_label($attr){
@@ -126,8 +109,6 @@ if($argc == 2 && $argv[1] == '--help'){
 if(trim(fgets(STDIN)) != '.IPPcode19'){
 	exit (HEAD_ERROR);
 }
-
-
 
 $glob_array = array(
 			array('CREATEFRAME', 'PUSHFRAME', 'POPFRAME',
@@ -148,31 +129,50 @@ $glob_array = array(
 while($x = trim(strtoupper(strtok(fgets(STDIN), ' ')))){
 	for ($i=0; $i<count($glob_array); $i++){
 		if (in_array($x, $glob_array[$i])){
-			instr_xml($x);
+			xml_instr($x);
 			switch ($i){
 				case 0:
 					break;
 				case 1:
 					if(var_attr(trim(next_attr()), 1) != OK)
-						return 22;
+						return ERR;
 					break;
 				case 2:
-					if(is_symb(trim(next_attr()), 1) != OK)
-						break;
+					if(symb_attr(trim(next_attr()), 1) != OK)
+						return ERR;
+					break;
 				case 3:
 					if(label_attr(trim(next_attr()), 1) != OK)
-						break;
+						return ERR;
+					break;
 				case 4:
+					if(var_attr(trim(next_attr()), 1) != OK)
+						return ERR;
+					if(symb_attr(trim(next_attr()), 2) != OK)
+						return ERR;
+					break;
 				case 5:
+					break;
 				case 6:
+					if(var_attr(trim(next_attr()), 1) != OK)
+						return ERR;
+					if(symb_attr(trim(next_attr()), 2) != OK)
+						return ERR;
+					if(symb_attr(trim(next_attr()), 3) != OK)
+						return ERR;
+					break;
 				case 7:
+					if(label_attr(trim(next_attr()), 1) != OK)
+						return ERR;
+					if(symb_attr(trim(next_attr()), 2) != OK)
+						return ERR;
+					if(symb_attr(trim(next_attr()), 3) != OK)
+						return ERR;
 					break;
 			}
 			xmlwriter_end_element($xw);
 		}
 	}
-	strtok('');
-	strtok('');
 }
 
 xmlwriter_end_element($xw); 		//program
